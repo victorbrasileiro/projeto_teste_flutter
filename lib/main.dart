aef3c46b34032ee0e6f32d3b597dbb5ffea2a403
Victor Brasileiro <brasileiro.marcello@hotmail.com>
2024-03-19 07:36:38 -0400

    Segunda atualização no projeto teste

--- lib/main.dart
+++ lib/main.dart
@@ -7,24 +7,95 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Container(
+          color: Colors.brown,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(
+                    color: Colors.white,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.red,
+                    width: 50,
+                    height: 50,
+                  ),
+                ],
+              ),
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(
+                    color: Colors.red,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.white,
+                    width: 50,
+                    height: 50,
+                  ),
+                ],
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      color: Colors.cyan,
+                      height: 50,
+                      width: 50,
+                    ),
+                    Container(
+                      color: Colors.pinkAccent,
+                      height: 50,
+                      width: 50,
+                    ),
+                    Container(
+                      color: Colors.purple,
+                      height: 50,
+                      width: 50,
+                    ),
+                  ]),
 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Stack(
-        alignment: AlignmentDirectional.center,
-        children: [
-          Container(color: Colors.red,width: 100, height: 100,),
-          Container(color: Colors.blue,width: 50, height: 50,),
-        ]
-      )
-    );
+              Container(
+                color: Colors.amber,
+                height: 30,
+                width: 300,
+                child: Text(
+                  'Diamante Amarelo',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 28,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    print('Você apertou o Botão');
+                  },
+                  child: Text('Aperte o botão!'),
+              ),
+            ],
+          ),
+
+        ));
+
+
   }
 }
-
